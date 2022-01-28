3e4ed7f1a1b6e371cd679372489d92e9b4a8870b
shumikhin <sashumihin@gmail.com>
2022-01-28 10:46:09 +0300

    Конец Главы 4, 150 стр.

--- src/Song.kt
+++ src/Song.kt
@@ -1,11 +1,23 @@
 class Song(val title: String, val artist: String) {
 
+    //Если при создании каждого объекта должен выполняться дополнительный
+    //код, можно использовать один или несколько блоков инициализации. Блоки инициализации
+    //выполняются при инициализации объекта сразу же после вызова конструктора и снабжаются префиксом init.
+    init {
+        println("Song $title has been created.")
+    }
+
     fun play() {
         println("Playing the song $title by $artist")
     }
 
     fun stop() {
         println("Stopped playing $title")
+    }
+
+    //Второй блок инициализации выполняется после создания свойств.
+    init {
+        println("Экземпляр класса Song $title создан!")
     }
 
 }

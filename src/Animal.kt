44e02d66e2e4e70539413a69a8b695014e262f0a
shumikhin <sashumihin@gmail.com>
2022-02-01 16:35:50 +0300

    Глава 5, 166 стр.

--- src/Animal.kt
+++ src/Animal.kt
@@ -1,2 +1,22 @@
-class Animal {
+//Супер класс, от которого все будут наследоваться
+open class Animal {
+
+    open val image = ""
+    open val food = ""
+    open val habitat = ""
+    var hunger = 10
+
+    open fun makeNoise() {
+        println("The Animal is making a noise")
+    }
+    open fun eat() {
+        println("The Animal is eating")
+    }
+    open fun roam() {
+        println("The Animal is roaming")
+    }
+    fun sleep() {
+        println("The Animal is sleeping")
+    }
+
 }
---
+++ src/Hippo.kt
@@ -0,0 +1,19 @@
+class Hippo : Animal() { //Это означает «класс Hippo является подтипом Animal».
+
+    override val image: String = "hippo.jpg"
+    override val food: String = "grass"
+    override val habitat: String = "water"
+
+    override fun makeNoise() {
+
+    }
+
+    override fun eat() {
+
+    }
+
+    override fun roam() {
+
+    }
+
+}

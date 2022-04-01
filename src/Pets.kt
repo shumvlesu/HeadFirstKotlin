b8e98e06999729a08fbc8cf5b46d238a6c507fe6
shumihin.s <sashumihin@gmail.com>
2022-04-01 10:35:42 +0300

    Глава 10, 335 стр. max неактуален и заменен на maxOrNull версии котлина 1.4. Книга по 1.3

---
+++ src/Pets.kt
@@ -0,0 +1,22 @@
+
+//Добавление иерархии Pet
+abstract class Pet(var name: String)
+class Cat(name: String) : Pet(name)
+class Dog(name: String) : Pet(name)
+class Fish(name: String) : Pet(name)
+
+//Добавление класса Contest (Выставка)
+class Contest<T: Pet> {
+    val scores: MutableMap<T, Int> = mutableMapOf()
+    fun addScore(t: T, score: Int = 0) {
+        if (score >= 0) scores.put(t, score)
+    }
+    fun getWinners(): MutableSet<T> {
+        val winners: MutableSet<T> = mutableSetOf()
+        val highScore = scores.values.maxOrNull()
+        for ((t, score) in scores) {
+            if (score == highScore) winners.add(t)
+        }
+        return winners
+    }
+}
---
+++ src/Start.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    //Создание 2х объектов Cat и одного Fish.
+    val catFuzz = Cat("Fuzz Lightyear")
+    val catKatsu = Cat("Katsu")
+    val fishFinny = Fish("Finny McGraw")
+
+    //Создание объекта Contest, предназначенного только для Cat.
+    val catContest = Contest<Cat>()
+    catContest.addScore(catFuzz, 50)
+    catContest.addScore(catKatsu, 45)
+    val topCat = catContest.getWinners().first()
+    println("Cat contest winner is ${topCat.name}")
+
+    //Создание объекта Contest для Pet;
+    //такой объект будет принимать любые подклассы Pet.
+    val petContest = Contest<Pet>()
+    petContest.addScore(catFuzz, 50)
+    petContest.addScore(fishFinny, 56)
+    val topPet = petContest.getWinners().first()
+    println("Pet contest winner is ${topPet.name}")
+
+}

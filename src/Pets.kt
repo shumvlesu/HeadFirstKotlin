2dceced9cd4058fde0d337e1b363454447d25c56
Sergey <sashumihin@gamil.com>
2022-04-04 07:12:07 +0300

    Глава 10, 345 стр.

--- src/Pets.kt
+++ src/Pets.kt
@@ -6,7 +6,8 @@ class Dog(name: String) : Pet(name)
 class Fish(name: String) : Pet(name)
 
 //Добавление класса Contest (Выставка)
-class Contest<T: Pet> {
+//Vet<T> добавляется в конструктор Contest, чтобы объект Contest нельзя было создать без назначения объекта Vet (ветеринар).
+class Contest<T: Pet>(var vet: Vet<T>) {
     val scores: MutableMap<T, Int> = mutableMapOf()
     fun addScore(t: T, score: Int = 0) {
         if (score >= 0) scores.put(t, score)
---
+++ src/Vet.kt
@@ -0,0 +1,8 @@
+//ветеринар может лечить только животных поэтому ограничим обобщение классом Pet
+//Префикс in в классе Vet означает, что Vet<Pet> может использоваться вместо Vet<Cat>, поэтому код успешно компилируется.
+//например - val catContest = Contest<Cat>(Vet<Pet>())
+class Vet<in T: Pet> {
+    fun treat(t: T) {
+        println("Treat Pet ${t.name}")
+    }
+}
